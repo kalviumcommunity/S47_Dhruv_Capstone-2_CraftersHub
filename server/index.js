1aa8070834a517bf5b0dee3a0a5d619ac330ce2b
Dhruv1184 <dhruvkhandelwal1184@gmail.com>
2024-07-03 13:09:58 +0530

    deployed in vercel

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
-const express = require('express')
-const mongoose = require("mongoose")
-const product = require('./routes/product')
-const auth = require('./routes/auth')
-const message = require('./routes/messages')
-const cors = require('cors')
-const {app, server} = require('./utils/socket')
-require('dotenv').config()
-const port = process.env.PORT || 2000
-
+const express = require('express');
+const mongoose = require('mongoose');
+const product = require('./routes/product');
+const auth = require('./routes/auth');
+const message = require('./routes/messages');
+const cors = require('cors');
+const { app, server } = require('./utils/socket');
+require('dotenv').config();
+const port = process.env.PORT || 2000;
 
 const allowedOrigins = [
-  // 'https://crafters-hub.netlify.app',
-  // 'http://localhost:5173',
-  'https://s47-dhruv-capstone-2-crafters-hub.vercel.app/'
+  'https://s47-dhruv-capstone-2-crafters-hub.vercel.app',
+  // other allowed origins can be added here
 ];
 
 const corsOptions = {
@@ -25,27 +23,30 @@ const corsOptions = {
     }
     return callback(null, true);
   },
-  credentials: true
+  credentials: true,
+  optionsSuccessStatus: 200, // For legacy browser support
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-app.use(express.json())
+app.options('*', cors(corsOptions)); // Enable pre-flight requests for all routes
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-  
-mongoose.connect(process.env.MONGOOSE_URL,{
-    dbName:"CraftersHub"
-}).then(()=>{
-    console.log("MongoDB connected");
-}).catch((err)=>{
-    console.log(err);
-})
 
-app.use('/product',product)
-app.use('/',auth)
-app.use('/message',message)
+mongoose.connect(process.env.MONGOOSE_URL, {
+  dbName: 'CraftersHub',
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.log(err);
+});
 
+app.use('/product', product);
+app.use('/', auth);
+app.use('/message', message);
 
-server.listen(port,()=>{
-    console.log(`Server is running on ${port}`);
-})
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
